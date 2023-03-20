140664e93684b9bc844b867b41120b919a1efcff
salt-dev <salt-dev@salt-devs-MacBook-Air-3.local>
2023-03-20 15:05:08 +0100

    we can access contentful, but not display in website

--- meerkats-nextjs/src/app/page.tsx
+++ meerkats-nextjs/src/app/page.tsx
@@ -1,33 +1,33 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "./page.module.css";
-const contentful = require("contentful");
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from './page.module.css'
+const contentful = require('contentful')
 
-const inter = Inter({ subsets: ["latin"] });
+
+const inter = Inter({ subsets: ['latin'] })
 
 const client = contentful.createClient({
-  space: "9yj1xgb6zgkj",
-  environment: "master", // defaults to 'master' if not set
-  accessToken: "LD3cBN2QhxQwzRhvuBJihPMfUu5dQ2Qgde_u3flr8FM",
-});
+  space: '9yj1xgb6zgkj',
+  environment: 'master', // defaults to 'master' if not set
+  accessToken: 'LD3cBN2QhxQwzRhvuBJihPMfUu5dQ2Qgde_u3flr8FM'
+})
 const getData = async () => {
-  await client
-    .getEntries()
+  await client.getEntries()
     .then((response: any) => {
-      console.log("", response.items);
-      const data = response;
-      return data.items;
-    })
-    .catch(console.error);
-};
+      console.log('', response.items)
+      return response.items
+      })
+    .catch(console.error)
+}
 
 export default async function Home() {
-  console.log("hello");
-  const data: any = await getData();
+  console.log('hello')
+  const data : any = await getData()
   return (
     <main>
       <h1>Products</h1>
       <p>{JSON.stringify(data)}</p>
+      
     </main>
-  );
+  )
 }
