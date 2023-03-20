62e1b25194487b59a4d0c4f837e85eff6115d640
salt-dev <salt-dev@salt-devs-MacBook-Air-3.local>
2023-03-20 16:18:48 +0100

    we are able to see data in the webpage

--- meerkats-nextjs/src/app/page.tsx
+++ meerkats-nextjs/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 const contentful = require('contentful')
@@ -11,22 +10,34 @@ const client = contentful.createClient({
   environment: 'master', // defaults to 'master' if not set
   accessToken: 'LD3cBN2QhxQwzRhvuBJihPMfUu5dQ2Qgde_u3flr8FM'
 })
+// const getData = async () => {
+//   await client.getEntries()
+//     .then((response: any) => {
+//       console.log('hello', response.items[0].fields)
+//       return response.items
+//       })
+//     .catch(console.error)
+// }
 const getData = async () => {
-  await client.getEntries()
-    .then((response: any) => {
-      console.log('', response.items)
-      return response.items
-      })
-    .catch(console.error)
+  try {
+    const response = await client.getEntries()
+    // console.log('hello', response.items[0].fields)
+    return response.items[0].fields
+  } catch (error) {
+    console.error(error)
+    return null
+  }
 }
 
+
 export default async function Home() {
-  console.log('hello')
   const data : any = await getData()
+  console.log(data.tShirt)
   return (
     <main>
       <h1>Products</h1>
-      <p>{JSON.stringify(data)}</p>
+      {/* {data.mug} */}
+      <p>{JSON.stringify(data.mug)}</p>
       
     </main>
   )
